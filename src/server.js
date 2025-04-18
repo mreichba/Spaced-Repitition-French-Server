923e216cddab4f5532f0a4d9d8dcc7f47bb36fee
Mason Reichbauer <mgreichba@gmail.com>
2025-04-18 16:05:38 -0400

    updated some dependencies

--- src/server.js
+++ src/server.js
@@ -7,10 +7,16 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
+  pool: {
+    min: 1,
+    max: 10, // try raising this if it's low
+  },
+  acquireConnectionTimeout: 5000 // 5 seconds
 })
 
 app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Connecting to DB: ${DATABASE_URL}`)
 })
