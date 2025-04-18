b534b43cb475b72662f9a0b7dcaf129d148b9b88
Mason Reichbauer <mreichba@mail.usf.edu>
2025-04-18 16:05:38 -0400

    updated some dependencies

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -3,15 +3,16 @@ BEGIN;
 TRUNCATE
   "word",
   "language",
-  "user";
+  "user"
+RESTART IDENTITY CASCADE;
 
+--password = pass
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
   (
     1,
     'admin',
     'Dunder Mifflin Admin',
-    -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
